8a3925e2b3d8b8cf553eaf1062b80dab107c28e7
Bruno Juchli <bruno.juchli@smart-me.com>
2022-11-22 12:01:55 +0100

    some dos and don'ts for OptionAsync

---
+++ OptionExamples/ConvertFromAndToNullable.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using LanguageExt;
+
+namespace OptionExamples;
+
+public class ConvertFromAndToNullable
+{
+    public Option<int> ConvertFromNullableToOption(int? someValue) 
+        => Prelude.Optional(someValue);
+    
+    public OptionAsync<int> ConvertFromNullableToOptionAsync(int? somevalue)
+        => Prelude.OptionalAsync(somevalue);
+
+    public IEnumerable? ConvertFromOptionToNullable(Option<IEnumerable> option)
+    {
+        // DON'T -- language ext will will throw an exception!
+        IEnumerable? result = option.IfNone(() => null!);
+
+        // DO - but before you do it: better to return the Option instead?
+        return (IEnumerable?)option.Case;
+    }
+    
+    public async Task<IEnumerable?> ConvertFromOptionAsyncToNullable(OptionAsync<IEnumerable> option)
+    {
+        // DON'T -- language ext will will throw an exception!
+        IEnumerable? result = await option.IfNone(() => null!);
+
+        // DO - but before you do it: better to return the Option instead?
+        return (IEnumerable?)await option.Case;
+    }
+}
---
+++ OptionExamples/OptionAsyncExamples.cs
@@ -0,0 +1,42 @@
+﻿using LanguageExt;
+
+namespace OptionExamples;
+
+public class OptionAsyncExamples
+{
+    public async Task<int> Awaiting_DONT(OptionAsync<int> option)
+    {
+        // DON'T
+        // this will throw an exception in case of Option<T>.None !
+        int result = await option;
+        return result;
+    }
+
+    public Task<int> Returning_Fallback_value(OptionAsync<int> option)
+    {
+        return option
+            .IfNone(int.MinValue);
+    }
+
+    public OptionAsync<string> Conversion(OptionAsync<int> option)
+    {
+        return option
+            .Map(val => val.ToString());
+    }
+
+    public OptionAsync<string> ChainingWithNextOption(OptionAsync<int> option)
+    {
+        return option
+            .Bind(
+                val => 
+                    val > 0
+                        ? val.ToString()
+                        : OptionAsync<string>.None);
+    }
+
+    public EitherAsync<string, int> NoneMeansError(OptionAsync<int> option)
+    {
+        return option
+            .ToEither("sorry but there's no value");
+    }
+}
